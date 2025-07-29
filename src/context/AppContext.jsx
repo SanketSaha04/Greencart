0a2868bcc8cfe641a6e5f7989790ff1711c019d0
SanketSaha04 <sanketsaha04@gmail.com>
2025-07-29 21:31:21 +0530

    Added Cart.jsx

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -52,8 +52,31 @@ export const AppContextProvider = ({children})=>{
             delete cartData[itemId];
         }
      }
+    toast.success("Removed from Cart")
      setCartItems(cartData)
-     toast.success("Removed from Cart")
+   
+   }
+   // Get Cart Item Count
+   const getCartCount =()=>{
+    let totalCount =0;
+    for (const item in cartItems){
+        totalCount += cartItems[item];
+        
+    }
+    return totalCount;
+   }
+
+   // Get Cart Total Amount
+
+   const getCartAmount =()=>{
+    let totalAmount=0;
+    for(const items in cartItems ){
+        let itemInfo =products.find((product)=>product._id === items);
+        if(cartItems[items]>0){
+            totalAmount += itemInfo.offerPrice * cartItems[items]
+        }
+    }
+    return Math.floor(totalAmount *100)/100;
    }
 
     //Fetch All product
@@ -67,7 +90,7 @@ export const AppContextProvider = ({children})=>{
 
 
 
-    const value={navigate,user,setUser,isSeller,setIsSeller,showUserLogin,setShowUserLogin,products,currency,addToCart,updateCartItem,removeFromCart,cartItems,searchQuery,setSearchQuery}
+    const value={navigate,user,setUser,isSeller,setIsSeller,showUserLogin,setShowUserLogin,products,currency,addToCart,updateCartItem,removeFromCart,cartItems,searchQuery,setSearchQuery,getCartCount,getCartCount,getCartAmount}
     return <AppContext.Provider value={value}>
         {children}
     </AppContext.Provider>
